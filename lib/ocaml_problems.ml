6dd0f40f7af55d013cc9c4b1bfaf4094901005b6
marcopist <pistilli.mrc@gmail.com>
2025-08-28 14:50:08 +0000

    Add Problem 15's description

--- lib/ocaml_problems.ml
+++ lib/ocaml_problems.ml
@@ -95,6 +95,12 @@ end
 
 module Problem15 : Problem = struct
 
+  (* Starting in the top left corner of a 2×2 grid, and only being able to move
+   to the right and down, there are exactly 6 routes to the bottom right
+   corner.
+
+   How many such routes are there through a 20×20 grid? *)
+
   let box_solution n = choose (n * 2) n
   let result = box_solution 20 |> string_of_int
 end
